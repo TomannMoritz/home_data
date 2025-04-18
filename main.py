31283929ea45205a53ff4bfc8d88e2b87cd8afda
TomannMoritz <164042654+TomannMoritz@users.noreply.github.com>
2025-04-18 18:32:51 +0200

    feat: query tibber for energy prices

--- main.py
+++ main.py
@@ -2,27 +2,54 @@ import os
 from dotenv import load_dotenv
 
 import miele_data
+import tibber_data
+
+import file
+import date_time
 
 MIELE_API_TOKEN_ = "MIELE_API_TOKEN"
+TIBBER_API_TOKEN_ = "TIBBER_API_TOKEN"
+CWD = ""
 
 
 def query_miele():
-    cwd = os.path.dirname(os.path.abspath(__file__)) + "/"
     miele_log_file = "miele.log"
-
-    miele_data.setup(cwd, miele_log_file, "")
+    miele_data.setup(CWD, miele_log_file, "")
 
     miele_api_token = os.getenv(MIELE_API_TOKEN_)
-    miele_data.query_device_information(miele_api_token)
+    result = miele_data.query_device_information(miele_api_token)
+
+    if result is None:
+        return False
+
+    file.update_file(CWD, miele_log_file, f"[+] {date_time.get_current_time()} Data received\n")
+    return True
+
+
+def query_tibber():
+    tibber_log_file = "tibber.log"
+    tibber_data.setup(CWD, tibber_log_file, "")
+
+    tibber_api_token = os.getenv(TIBBER_API_TOKEN_)
+    result = tibber_data.query_device_information(tibber_api_token, tibber_data.get_query_price_info_today())
+
+    if result is None:
+        return False
+    file.update_file(CWD, tibber_log_file, f"[+] {date_time.get_current_time()} Data received\n")
+    return True
 
 
 def main():
-    print("START")
+    global CWD
+    CWD = os.path.dirname(os.path.abspath(__file__)) + "/"
 
     login_name = os.getlogin()
     load_dotenv(f"/home/{login_name}/.config/.env")
 
-    query_miele()
+    # TODO: requery with exponential backoff
+    miele_successfull = query_miele()
+    print("-------")
+    tibber_successfull = query_tibber()
 
 
 if __name__ == "__main__":
---
+++ tibber_data.py
@@ -0,0 +1,72 @@
+import json
+
+import rest_query
+import file
+import date_time
+
+CWD = ""
+LOG_FILE = ""
+TIBBER_URL = "https://api.tibber.com/v1-beta/gql"
+
+
+def setup(cwd, log_file, api_url):
+    global CWD, LOG_FILE, TIBBER_URL
+    CWD = cwd
+    LOG_FILE = log_file
+    if api_url is not None and api_url != "":
+        TIBBER_URL = api_url
+
+
+def get_query_price_info_today():
+    return """ {
+            viewer {
+                homes {
+                    currentSubscription {
+                        priceInfo {
+                            today {
+                                total
+                                startsAt
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+    """
+
+
+def get_extracted_data(tibber_data, day="today"):
+    data_today = tibber_data["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"][day]
+    return data_today
+
+
+def get_price_array(extracted_data):
+    return [price["total"] for price in extracted_data]
+
+
+def query_device_information(api_token, price_info_query):
+    if api_token is None or not api_token:
+        file.update_file(CWD, LOG_FILE, f"[!] {date_time.get_current_time()} API TOKEN MISSING\n")
+        return
+
+    if TIBBER_URL is None or not TIBBER_URL:
+        file.update_file(CWD, LOG_FILE, f"[!] {date_time.get_current_time()} API URL MISSING\n")
+        return
+
+    query_body = json.dumps({"query": price_info_query})
+
+    # Request data
+    curl_command = ["curl",
+                    "-X", "POST",
+                    TIBBER_URL,
+                    "-H",  f"Authorization: Bearer {api_token}",
+                    "-H", "Content-Type: application/json",
+                    "-d", query_body
+                    ]
+
+    result = rest_query.execute_curl_query(curl_command, CWD, LOG_FILE)
+
+    data_today = get_extracted_data(result)
+    return data_today
+
+
