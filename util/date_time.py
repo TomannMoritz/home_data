c11ac378b9b18a8c8b49deeeaf5a0abbae6397d1
TomannMoritz <164042654+TomannMoritz@users.noreply.github.com>
2025-04-22 15:14:51 +0200

    fix: date_time - use local time

--- util/date_time.py
+++ util/date_time.py
@@ -1,18 +1,5 @@
-from time import gmtime, strftime, time
-
-HOURS_AHEAD = 0
-MINUTES_A_HOUR = 60
-SECONDS_A_MINUTE = 60
+from time import localtime, strftime
 
 
 def get_current_time():
-    curr_time = time() + HOURS_AHEAD * MINUTES_A_HOUR * SECONDS_A_MINUTE
-    return strftime("%Y-%m-%d %H:%M:%S", gmtime(curr_time))
-
-
-def get_forecast_file_path():
-    return strftime("%Y_%m_%d", gmtime())
-
-
-def get_current_file_path():
-    return strftime("%Y_%m", gmtime())
+    return strftime("%Y-%m-%d %H:%M:%S", localtime())
