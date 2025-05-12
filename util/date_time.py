16774a351ce4593f38ddb53a2a25e27ee5002306
TomannMoritz <164042654+TomannMoritz@users.noreply.github.com>
2025-05-12 15:12:22 +0200

    fix: calcualting next quarter times

--- util/date_time.py
+++ util/date_time.py
@@ -9,14 +9,16 @@ def get_hour_minute():
     return int(strftime("%H", localtime())), int(strftime("%M", localtime()))
 
 
-def get_next_quarter(hour, minute):
-    remaining = minute % 15
-    minute += 15 - remaining
-    minute %= 60
+def get_next_quarter(curr_hour, curr_minute):
+    assert curr_hour < 24, f"Invalid input: {curr_hour=}"
+    assert curr_minute < 60, f"Invalid input: {curr_minute=}"
 
-    if remaining != 0 and minute % 60 == 0:
-        return (hour + 1) % 24, minute
-    return hour, minute
+    remaining = 15 - (curr_minute % 15)
+    minute = curr_minute + remaining
+
+    if minute >= 60:
+        return (curr_hour + 1) % 24, 0
+    return curr_hour, minute
 
 
 def get_index_quarter(hour, minute):
